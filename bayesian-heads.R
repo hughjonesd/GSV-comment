a5003d3a92e5823a81fe4c64237d7b46a4ae892f
David Hugh-Jones <davidhughjones@gmail.com>
2018-10-31 10:39:30 +0000

    document bayesian-heads.R

--- bayesian-heads.R
+++ bayesian-heads.R
@@ -43,10 +43,11 @@ prob_report_given_L <- function (L, heads, N, P) {
 #'   `prior[1]` is the prior probability that L == 0
 #'   `prior[2]` is the prob that L == 1, etc.
 #'
-#' @return
+#' @return A vector of posterior probabilities
 #' @export
 #'
 #' @examples
+#' probs_0_to_10 <- prob_L_given_report(0:10, 13, 20, 0.5, uniform_prior(20))
 prob_L_given_report <- function(L, heads, N, P, prior) {
   check_posterior(prior)
   stopifnot(length(prior) == N + 1)
@@ -59,8 +60,20 @@ prob_L_given_report <- function(L, heads, N, P, prior) {
   return(numerator/denominator)
 }
 
-uniform_prior <- function (N) rep(1/(N + 1), N + 1)
-
+#' Uniform prior on 0 to N
+#' 
+#' Creates a prior with equal probability of every number from 0 to N inclusive.
+#'
+#' @param N sample size
+#'
+#' @return The prior
+#' @export
+#'
+#' @examples
+#' uniform_prior(5)
+uniform_prior <- function (N) {
+  rep(1/(N + 1), N + 1)
+}
 
 #' Update prior distribution given heads/N heads reported
 #' 
@@ -69,10 +82,19 @@ uniform_prior <- function (N) rep(1/(N + 1), N + 1)
 #' @param prior By default, a uniform prior on there being between 
 #'   0 and N liars inclusive.
 #'
-#' @return
+#' @return The updated posterior
 #' @export
 #'
 #' @examples
+#' updated <- update_prior(33, 50, 0.5)
+#' posterior_mean(updated)
+#' posterior_quantile(c(.025, .975), updated)
+#' barplot(updated)
+#' 
+#' # different prior:
+#' updated <- update_prior(33, 50, 0.5, prior = dbinom(0:50, 50, 0.25))
+#' posterior_mean(updated)
+#' posterior_quantile(c(.025, .975), updated)
 update_prior <- function(heads, N, P, prior = uniform_prior(N)) {
   pstr <- prob_L_given_report(0:N, heads = heads, N = N, P = P,
         prior = prior)
@@ -86,13 +108,32 @@ check_posterior <- function(pstr) {
   stopifnot(isTRUE(all.equal(sum(pstr), 1)), all(pstr >= 0))
 }
 
+#' Returns the mean of a discrete distribution
+#'
+#' @param pstr A vector of positive numbers summing to 1
+#'
+#' @return
+#' @export
+#'
+#' @examples
 posterior_mean <- function (pstr) {
   check_posterior(pstr)
   
   sum(pstr * seq(0, along.with = pstr))
 }
 
+
+#' Returns quantiles of a discrete distribution
+#'
+#' @param q A vector of quantiles
+#' @param pstr A vector of positive numbers summing to 1
+#'
+#' @return A vector of values at the quantiles
+#' @export
+#'
+#' @examples
 posterior_quantile <- function (q, pstr) {
+  stopifnot(all(q >= 0), all(q <= 1))
   check_posterior(pstr)
   cdf <- cumsum(pstr) # prob L <= l, for l = 0 to N
 
