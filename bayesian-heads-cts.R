6dc8f866e62282a44baaa21b20c7b5a50898028b
David Hugh-Jones <davidhughjones@gmail.com>
2018-11-02 10:22:30 +0000

    New difference_dist and dist_quantile functions

--- bayesian-heads-cts.R
+++ bayesian-heads-cts.R
@@ -2,7 +2,6 @@
 # Bayesian ways to calculate the probability of people in a sample reporting heads 
 # when they observe tails
 
-is_prob <- function (p) all(p >= 0 & p <= 1)
 
 #' Calculate the probability of observing `heads`
 #' good outcomes out of `N` total outcomes, conditional
@@ -56,16 +55,113 @@ update_prior <- function(heads, N, P, prior, npoints = 1e3) {
   return(posterior)
 }
 
-mean_dist <- function (dist) {
+
+
+#' Estimate probability that two samples s1, s2 from 2 independent distributions have s1 > s2
+#'
+#' @param dist1 
+#' @param dist2 
+#'
+#' @return A probability scalar
+#' @export
+#'
+#' @examples
+compare_dists <- function (dist1, dist2) {
+  outer_f <- function (l1) {
+    dist1(l1) * dist_prob_within(dist2, 0, l1)
+  }
+  outer_f <- Vectorize(outer_f)
+  prob_1_more <- try_integral(outer_f, 0, 1)
+  
+  return(prob_1_more)
+}
+
+
+#' Given dist1 and dist2, find the pdf of dist1 - dist2
+#' 
+#' At the moment this only works when dist1 and dist2 are defined on [0, 1].
+#' 
+#' @param dist1,dist2 Probability distribution functions 
+#'
+#' @return A probability distribution function defined on [-1, 1].
+#' 
+#' @examples
+difference_dist <- function (dist1, dist2) {
+  
+  # prob l1 - l2 = d is integral from 0 to 1-d of dist1(z+d)*dist2(z)
+  # l1 - l2 = -.5; l1 = l2 + .5; integral from .5 to 1 of dist1(z + d)*dist2(z)
+  dd_fun <- function (d) {
+    stopifnot(d >= -1 && d <= 1)
+    dist_prob <- function(x) dist1(x + d) * dist2(x)
+    dist_prob <- Vectorize(dist_prob)
+    ends <- if (d > 0) c(0, 1 - d) else c(-d, 1)
+    try_integral(dist_prob, ends[1], ends[2])
+  }
+  
+  return(Vectorize(dd_fun))
+}
+
+
+#' Find mean of a probability distribution function
+#'
+#' @param dist A one-argument function
+#' @param l Minimum value
+#' @param r Maximum value
+#'
+#' @return A scalar
+#'
+#' @examples
+dist_mean <- function (dist, l = 0, r = 1) {
   stopifnot(is.function(dist))
   
   f <- function (x) x * dist(x)
-  EV <- try_integral(f, 0, 1)
+  EV <- try_integral(f, l, r)
 
   return(EV)
 }
 
-interval_prob <- function (dist, l, r) {
+dist_prob_within <- function (dist, l, r) {
   stopifnot(l <= r, is.function(dist))
   try_integral(dist, l, r)
+}
+
+
+#' Find quantiles given a probability distribution function
+#'
+#' @param dist A one argument function
+#' @param probs A vector of probabilities
+#'
+#' @return A vector of quantiles
+#'
+#' @examples
+dist_quantile <- function(dist, probs) {
+  stopifnot(is_prob(probs), is.function(dist))
+  
+  qs <- sapply(probs, function (prob) {
+    f <- function (x) (dist_prob_within(dist, 0, x) - prob)^2
+    # want x s.t. interval_prob(dist, 0, x) == prob
+    res <- optimize(f, c(0, 1))
+    res$minimum
+  })
+  
+  return(qs)
+}
+
+is_prob <- function (p) all(p >= 0 & p <= 1)
+
+
+if (FALSE) {
+  N <- 100
+  P <- 0.5
+  prior <- dunif
+  CI <- 0.95
+  cov_check <- replicate(1000, {
+    lambda <- runif(1)
+    heads <- rbinom(1, N, p = lambda * P + 1 - P)
+    posterior <- update_prior(heads, N, P, prior)
+    cis <- dist_quantiles(posterior, c(1/2 - CI/2, 1/2 + CI/2))
+    
+    cis[1] <= lambda && lambda <= cis[2]
+  })
+  table(cov_check)
 }
